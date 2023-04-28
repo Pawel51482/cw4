8a8995c8db374d3b287db4a48bf7c4005f5ba68e
Pawel51482 <dsw51482@student.dsw.edu.pl>
2023-04-28 16:46:27 +0200

    pomoc

---
+++ skrypt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [[ $1 == "--help" ]]; then
+    echo "Mozliwe opcje:"
+    echo "--date #wyswietla aktualna date"
+    echo "--logs <liczba>  (bez podania liczby tworzy 100)"
+    echo "--help #wysweitla pomoc"
+fi
